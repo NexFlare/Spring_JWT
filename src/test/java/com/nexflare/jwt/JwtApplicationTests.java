d6f9a58bb363898efceda96d0b5fe62b2bdbe7b4
NexflareNEU <agarwal.hars@northeastern.edu>
2023-04-03 19:25:00 -0400

    Initial commit :tada:

---
+++ src/test/java/com/nexflare/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexflare.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
